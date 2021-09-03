47a12947d43088094b8631f0da71f3692f5da8e0
Pedro Bufulin <pedro.almeida@ufu.br>
2021-09-03 13:40:50 -0300

    feat(pages/activities): containers for future component positioning

--- src/pages/activities/index.tsx
+++ src/pages/activities/index.tsx
@@ -1,8 +1,64 @@
+import {
+  Container,
+  Header,
+  CardContainer,
+  Footer
+} from '@styles/pages/activities';
+
 function Home() {
   return (
-    <>
-      <h1>Logged in</h1>
-    </>
+    <Container>
+      <Header>
+        <div>
+          <p> activity select</p>
+        </div>
+        <div>
+          <p> card sumamry</p>
+        </div>
+        <div>
+          <p> filter</p>
+        </div>
+      </Header>
+      <div style={{ alignSelf: 'flex-end' }}>
+        <p> select all</p>
+      </div>
+
+      <CardContainer>
+        <div>
+          <h2>card</h2>
+        </div>
+        <div>
+          <h2>card</h2>
+        </div>
+        <div>
+          <h2>card</h2>
+        </div>
+        <div>
+          <h2>card</h2>
+        </div>
+        <div>
+          <h2>card</h2>
+        </div>
+        <div>
+          <h2>card</h2>
+        </div>
+        <div>
+          <h2>card</h2>
+        </div>
+        <div>
+          <h2>card</h2>
+        </div>
+        <div>
+          <h2>card</h2>
+        </div>
+      </CardContainer>
+
+      <Footer>
+        <div>
+          <h2> pages</h2>
+        </div>
+      </Footer>
+    </Container>
   );
 }
 
