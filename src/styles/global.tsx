2d601ba892ddb30bd02624274decc52f689d07a7
Pedro Bufulin <pedro.almeida@ufu.br>
2021-09-03 08:36:44 -0300

    refactor(assets): moved assets to public. Works better with imports in nextJS

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,6 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+   @font-face {
+    font-family: 'TitilliumWeb';
+     src: url('/assets/fonts/TitilliumWeb-Regular.ttf');
+    }
+    @font-face {
+    font-family: 'TitilliumWeb';
+     src: url('/assets/fonts/TitilliumWeb-Bold.ttf');
+     font-weight: bold;
+    }
+    @font-face {
+    font-family: 'TitilliumWeb';
+     src: url('/assets/fonts/TitilliumWeb-Light.ttf');
+     font-weight: lighter;
+    }
+    *{
+        font-family: TitilliumWeb;
+    }
 `;
 
 export default GlobalStyle;
